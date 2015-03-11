a087d0c8b37bf5fada72eb5fcd212fe166de3755
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 21:41:04 +0000

    version change
    git-svn-id: https://plugins.svn.wordpress.org/info-boxes-shortcode-and-widget/trunk@1110632 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- otw_content_manager.php
+++ otw_content_manager.php
@@ -4,7 +4,7 @@ Plugin Name: Info Boxes Shortcode And Widgets
 Plugin URI: http://OTWthemes.com
 Description:  Create Inbo Boxes. Nice and easy interface. Insert anywhere in your site - page/post editor, sidebars, template files.
 Author: OTWthemes.com
-Version: 1.2
+Version: 1.3
 
 Author URI: http://themeforest.net/user/OTWthemes
 */
